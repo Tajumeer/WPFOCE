dd1f3ecfc087dff9beffcef2ba2111db04ab498f
Sven <becker@hotmail.com>
2023-05-04 14:32:37 +0200

    initial

---
+++ WPFOCE/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFOCE
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml0
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string TextBoxContent { get; set; }
+        public MainWindow()
+        {
+            TextBoxContent = "Content";
+            InitializeComponent();
+        }
+
+        private void MyClick(object sender, RoutedEventArgs e)
+        {
+            ViewTextbox2.Text = TextBoxContent;
+        
+        }
+    }
+}
---
+++ WaveEditor/Wave.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WaveEditor
+{
+    [Serializable]
+    public class Wave
+    { 
+        private float timer;
+
+        private List<Enemy> enemyTypes = new List<Enemy>();
+
+        private Vector2 spawnpoint;
+    }
+
+    [Serializable]
+    public class Enemy
+    {
+        public String Name;
+        public int HealthPoints;
+    }
+
+    [Serializable]
+    public class WaveList
+    {
+        public List<Wave> Waves = new List<Wave>();
+    }
+}
